141426a2e91c86975eaa2587cb990d9932ac27f9
Thomas Spura <thomas.spura@gmail.com>
2015-10-21 14:06:18 +0200

    napoleon extension is named sphinxcontrib.napoleon according to pip

--- doc-sphinx/conf.py
+++ doc-sphinx/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('..'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
-    'sphinx.ext.napoleon',
+    'sphinxcontrib.napoleon',
     'sphinxcontrib.bibtex'
 ]
 
