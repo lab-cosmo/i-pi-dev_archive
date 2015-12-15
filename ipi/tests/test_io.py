f33967a18f59667ed5f139450e383ddc6de78d64
Thomas Spura <thomas.spura@gmail.com>
2015-12-15 15:29:43 +0100

    Adjust test names and remove two identical tests

--- ipi/tests/test_io.py
+++ ipi/tests/test_io.py
@@ -63,8 +63,8 @@ def test_iter_pdb():
             assert_equal(pos*(num+1), atoms.q)
 
 
-def test_print_pdb():
-    """Tests that pdb files are printed correctly."""
+def test_print_pdb2xyz():
+    """Tests that pdb->xyz files are printed correctly."""
 
     with open(local("test.pos_0.pdb"), "r") as f:
         with open(local("test.pos_1.xyz"), "w") as out:
@@ -79,8 +79,8 @@ def test_print_pdb():
     os.unlink(local("test.pos_1.xyz"))
 
 
-def test_print_xyz():
-    """Tests that xyz files are printed correctly."""
+def test_print_pdb2pdb():
+    """Tests that pdb->pdb files are printed correctly."""
 
     with open(local("test.pos_0.pdb"), "r") as f:
         with open(local("test.pos_1.pdb"), "w") as out:
@@ -134,35 +134,3 @@ def test_iter_pdb2():
             atoms = ret["atoms"]
             assert len(atoms) == 3
             assert_equal(pos*(num+1), atoms.q)
-
-
-def test_print_pdb2():
-    """Tests that mode/pdb files are printed correctly."""
-
-    with open(local("test.pos_0.pdb"), "r") as f:
-        with open(local("test.pos_1.xyz"), "w") as out:
-            for num, ret in enumerate(iter_file("pdb", f)):
-                atoms = ret["atoms"]
-                assert len(atoms) == 3
-                assert_equal(pos*(num+1), atoms.q)
-                print_file("xyz", atoms, ret["cell"], filedesc=out,
-                           title=ret["comment"])
-
-    assert filecmp.cmp(local("test.pos_0.xyz"), local("test.pos_1.xyz"))
-    os.unlink(local("test.pos_1.xyz"))
-
-
-def test_print_xyz2():
-    """Tests that mode/xyz files are printed correctly."""
-
-    with open(local("test.pos_0.pdb"), "r") as f:
-        with open(local("test.pos_1.pdb"), "w") as out:
-            for num, ret in enumerate(iter_file("pdb", f)):
-                atoms = ret["atoms"]
-                assert len(atoms) == 3
-                assert_equal(pos*(num+1), atoms.q)
-                print_file("pdb", atoms, ret["cell"], filedesc=out,
-                           title=ret["comment"])
-
-    assert filecmp.cmp(local("test.pos_0.pdb"), local("test.pos_1.pdb"))
-    os.unlink(local("test.pos_1.pdb"))
