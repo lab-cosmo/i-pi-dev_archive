9c02958d8a2504feaabcf98d0cb91d9db0654afb
Riccardo Petraglia <riccardo.petraglia@gmail.com>
2016-09-15 10:39:01 +0200

    Do not use profiling in usual run

--- env.sh
+++ env.sh
@@ -3,4 +3,7 @@ ENV_BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export PATH=$ENV_BASE_DIR/bin:$PATH
 export PYTHONPATH=$ENV_BASE_DIR:$PYTHONPATH
 
+# Avoid profiling in production situations
+export PYTHONOPTIMIZE=2
+
 unset ENV_BASE_DIR
